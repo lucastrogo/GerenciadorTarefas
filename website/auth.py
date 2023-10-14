30bbf57eff77af0231269a49693ac7363a2bb51d
Alanna <alanna.simoes@gmail.com>
2023-10-13 23:47:55 -0300

    consertando erro 404

--- website/auth.py
+++ website/auth.py
@@ -8,7 +8,7 @@ from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login', methods=['GET', 'POST'])
+@auth.route('/', methods=['GET', 'POST'])
 def login():
     '''Função para login de usuários já cadastrados'''
     

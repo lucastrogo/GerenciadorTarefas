c5ce39f4e71d9283c58931f47bbaf3a7e3dc585d
Alanna <alanna.simoes@gmail.com>
2023-10-16 20:38:14 -0300

    adicionando funcoes de excluir

--- website/views.py
+++ website/views.py
@@ -21,13 +21,19 @@ def adicionar_materia():
     db.session.add(materia)
     db.session.commit()
     flash('Matéria adicionada com sucesso!', 'success')
-    return redirect(url_for('logado'))
+    return redirect(url_for('views.logado'))
 
 @login_required
 @views.route('/materia/<int:id_materia>')
 def materia(id_materia):
     materia = Materia.query.get(id_materia)
     return render_template('materia.html', materia=materia)
+
+@login_required
+@views.route('/topico/<int:id_topico>')
+def topico(id_topico):
+    topico = Topico.query.get(id_topico)
+    return render_template('topico.html', topico=topico)
 
 @login_required
 @views.route('/adicionar_topico/<int:id_materia>', methods=['POST'])
@@ -37,7 +43,7 @@ def adicionar_topico(id_materia):
     db.session.add(topico)
     db.session.commit()
     flash('Tópico adicionado com sucesso!', 'success')
-    return redirect(url_for('materia', id_materia=id_materia))
+    return redirect(url_for('views.materia', id_materia=id_materia))
 
 @login_required
 @views.route('/marcar_completo/<int:id_topico>', methods=['POST'])
@@ -46,7 +52,7 @@ def marcar_completo(id_topico):
     topico.completo = True
     db.session.commit()
     flash('Tópico marcado como concluído!', 'success')
-    return redirect(url_for('materia', id_materia=topico.id_materia))
+    return redirect(url_for('views.materia', id_materia=topico.id_materia))
 
 @login_required
 @views.route('/adicionar_anotacao/<int:id_topico>', methods=['POST'])
@@ -56,4 +62,52 @@ def adicionar_anotacao(id_topico):
     db.session.add(anotacao)
     db.session.commit()
     flash('Anotação adicionada com sucesso!', 'success')
-    return redirect(url_for('materia', id_materia=Topico.query.get(id_topico).id_materia))
+    return redirect(url_for('views.topico', id_topico=id_topico))
+
+@login_required
+@views.route('/excluir_topico/<int:id_topico>', methods=['POST'])
+def excluir_topico(id_topico):
+    topico = Topico.query.get(id_topico)
+    
+    if topico:
+        # Exclua o tópico do banco de dados
+        db.session.delete(topico)
+        db.session.commit()
+        flash('Tópico excluído com sucesso!', 'success')
+    else:
+        flash('Tópico não encontrado', 'danger')
+    
+    # Redirecione de volta para a página de detalhes da matéria
+    return redirect(url_for('views.materia', id_materia=topico.id_materia))
+
+@login_required
+@views.route('/excluir_anotacao/<int:id_anotacao>', methods=['POST'])
+def excluir_anotacao(id_anotacao):
+    anotacao = Anotacao.query.get(id_anotacao)
+    
+    if anotacao:
+        # Exclua o tópico do banco de dados
+        db.session.delete(anotacao)
+        db.session.commit()
+        flash('Anotação excluída com sucesso!', 'success')
+    else:
+        flash('Anotação não encontrada', 'danger')
+    
+    # Redirecione de volta para a página de detalhes da matéria
+    return redirect(url_for('views.topico', id_topico=anotacao.id_topico))
+
+@login_required
+@views.route('/excluir_materia/<int:id_materia>', methods=['POST'])
+def excluir_materia(id_materia):
+    materia = Materia.query.get(id_materia)
+    
+    if materia:
+        # Exclua o tópico do banco de dados
+        db.session.delete(materia)
+        db.session.commit()
+        flash('Matéria excluída com sucesso!', 'success')
+    else:
+        flash('Matéria não encontrada', 'danger')
+    
+    # Redirecione de volta para a página de detalhes da matéria
+    return redirect(url_for('views.logaedo'))
